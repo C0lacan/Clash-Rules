68999e634734e4af95456c6008df6a5da10ef3d2
Zero Clover <hmsxcxy@vip.qq.com>
2019-02-24 23:06:42 +0800

    feat: redir and dns

--- clash_general.php
+++ clash_general.php
@@ -3,7 +3,7 @@
 return [
     'port'                => 8888,
     'socks-port'          => 8889,
-    'redir-port'          => 0,
+    'redir-port'          => 9090,
     'allow-lan'           => true,
     'mode'                => 'Rule',
     'log-level'           => 'info',
@@ -13,4 +13,21 @@ return [
     	'music.163.com',
     	'*.music.126.net',
     ],
+    'dns' => [
+        'enable'          =>  true,
+        'ipv6'            =>  false,
+        'listen'          =>  '0.0.0.0:53',
+        'enhanced-mode'   =>  'redir-host',
+        'nameserver'      => [
+            '119.29.29.29',
+            '119.28.28.28',
+            '1.2.4.8',
+            '223.5.5.5',
+            'tls://dns.rubyfish.cn:853'
+        ],
+        'fallback'       =>  [
+            'tls://1dot1dot1dot1.cloudflare-dns.com',
+            'tls://dns.google'
+        ]
+    ]
 ];
