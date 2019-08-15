6bc67e1298b4874a456509d2d5c5361d272bffb7
Mu <i-mu@tuta.io>
2019-08-15 08:31:13 +0000

    Update clash_general.php
    remove cfw-bypass

--- clash_general.php
+++ clash_general.php
@@ -9,8 +9,4 @@ return [
     'log-level'           => 'info',
     'external-controller' => '127.0.0.1:6170',
     'secret'              => '',
-    'cfw-bypass'          => [
-    	'music.163.com',
-    	'*.music.126.net',
-    ],
 ];
